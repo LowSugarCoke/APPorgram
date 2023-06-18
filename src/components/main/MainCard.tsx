ac72af90f570986ff3df81bfa587ad4fc15255c7
Jack Lee <jack20499@gmail.com>
2023-06-18 19:52:20 +0800

    update card style

--- src/components/main/MainCard.tsx
+++ src/components/main/MainCard.tsx
@@ -1,28 +1,38 @@
 import { Card, Button } from 'react-bootstrap';
+import styles from './MainCard.module.css';
 
-const MainCard = () => {
+interface Prop {
+    imgLink?: string;
+    title?: string;
+    content?: string;
+    link?: string;
+}
+
+const MainCard = ({ imgLink, title, content, link }: Prop) => {
     return (
-        <Card style={{ width: '18rem' }}>
-            <Card.Header as='h3'>
-                <a href="https://branchprogramming.com/2022/01/30/apcs%e7%b7%9a%e4%b8%8a%e8%a9%a6%e8%81%bd%e8%aa%b2%e7%a8%8b%e9%96%8b%e5%a7%8b%e5%a0%b1%e5%90%8d%e4%ba%86%ef%bc%81/">APCS線上試聽課程開始報名了！</a>
-                <div className="entry-meta">
-                    <span className="posted-on">
-                        <time className="entry-date published updated" dateTime="2022-01-30T11:21:17+08:00" itemProp="datePublished">2022-01-30</time>
-                        <time className="updated" dateTime="2022-01-30T11:22:56+08:00" itemProp="dateModified">2022-01-30</time>
-                    </span>
-                    <span className="comment-box">
-                        <a href="https://branchprogramming.com/2022/01/30/apcs%e7%b7%9a%e4%b8%8a%e8%a9%a6%e8%81%bd%e8%aa%b2%e7%a8%8b%e9%96%8b%e5%a7%8b%e5%a0%b1%e5%90%8d%e4%ba%86%ef%bc%81/#respond">No Comment</a>
+        <Card className={styles.myCardStyle}>
+            <Card.Header as='h3' className={styles.header}>
+                <a href={link} >
+                    <div
+                        className={styles.imageContainer}
+                        style={{ backgroundImage: `url(${imgLink})` }}
+                    />
+                    <span className={styles.title}>{title}</span>
+                </a>
+                <div className={styles.meta}>
+                    <span >
+                        <time dateTime="2022-01-30T11:21:17+08:00" itemProp="datePublished">2022-01-30</time>
                     </span>
                 </div>
             </Card.Header>
-            <Card.Body>
+            <Card.Body className={styles.body}>
                 <Card.Text>
-                    APCS線上免費試聽課程： 時間： 2/13(日)13:30~16:30 上課方式：google meet 課程內容：主要會介紹APCS，並帶同學寫 3~4屆APCS第一題，歡迎想要考APCS卻不太了解的同學報名！
+                    {content}
                 </Card.Text>
-                <Button variant="primary" href="https://branchprogramming.com/2022/01/30/apcs%e7%b7%9a%e4%b8%8a%e8%a9%a6%e8%81%bd%e8%aa%b2%e7%a8%8b%e9%96%8b%e5%a7%8b%e5%a0%b1%e5%90%8d%e4%ba%86%ef%bc%81/">Read More</Button>
+                <Button variant="primary" className={styles.button} href={link}>Read More</Button>
             </Card.Body>
         </Card>
     );
 }
 
-export default MainCard
+export default MainCard;
--- src/components/main/MainContent.tsx
+++ src/components/main/MainContent.tsx
@@ -3,8 +3,37 @@ import MainCard from "./MainCard"
 import MainBanner from "./MainBanner"
 import styles from "./MainContent.module.css"
 
+// 定義你的CardInfo接口
+interface CardInfo {
+    imgLink: string;
+    title: string;
+    content: string;
+    link: string;
+}
 
 const MainContent = () => {
+    // 定義你的卡片資料
+    const cardData: CardInfo[] = [
+        {
+            imgLink: "../../../public/card-1.jpg",
+            title: "APCS線上試聽課程開始報名了！",
+            content: "APCS線上免費試聽課程： 時間： 2/13(日)13:30~16:30 上課方式：google meet 課程內容：主要會介紹APCS，並帶同學寫 3~4屆APCS第一題，歡迎想要考APCS卻不太了解的同學報名！ ~~~~~報名連結~~~~~",
+            link: "/register"
+        },
+        {
+            imgLink: "/card-2.jpg",
+            title: "家教小團班開始招生拉~~",
+            content: "歡迎直接打電話詢問，或者直接填寫報名表，由我們直接聯繫您！ Loading…",
+            link: "/register"
+        },
+        {
+            imgLink: "/card-3.jpg",
+            title: "2021/11月APCS開始開始報名",
+            content: "報名日程：*10月01日（五）起至 10月04日（一）止每日9:00-18:00，限高中（職）三年級生報名。*10月05日（二）起至 10月08日（五）止每日9:00-18:00，限高中（職）各年級生報名。*10月09日（六）起至 10月11日（一）止每日9:00-18:00，不限對象。",
+            link: "/register"
+        },
+    ];
+
     return (
         <>
             <Container fluid className={styles.mainContainer}>
@@ -12,19 +41,13 @@ const MainContent = () => {
                     <Col className={styles.bannerCol}>
                         <MainBanner />
                     </Col>
-
                 </Row>
-
                 <Row className={styles.mainCardRow}>
-                    <Col className={styles.mainCardCol}>
-                        <MainCard />
-                    </Col>
-                    <Col className={styles.mainCardCol} >
-                        <MainCard />
-                    </Col>
-                    <Col className={styles.mainCardCol}>
-                        <MainCard />
-                    </Col>
+                    {cardData.map((data, index) => (
+                        <Col className={styles.mainCardCol} key={index}>
+                            <MainCard {...data} />
+                        </Col>
+                    ))}
                 </Row>
             </Container>
         </>
